1dfd7c0d73bd73383b4d3fb9c3d3a77517dffadf
Subhabrata Barik <subhabratab@vconstruct.in>
2026-05-29 20:46:51 +0530

    your commit message

--- Gymnastic/apps/contact/views.py
+++ Gymnastic/apps/contact/views.py
@@ -36,7 +36,7 @@ This message was automatically sent from the contact form on Bendre's Gymnastics
             
             # If no recipients are configured, fall back to default
             if not recipient_list:
-                recipient_list = ['bariksubhabrata945gmail.com', 'bendregymnasticclub@gmail.com']
+                recipient_list = ['bariksubhabrata945gmail.com', ' tejasbendre92@gmail.com']
             
             try:
                 send_mail(subject, message, from_email, recipient_list, fail_silently=False)
